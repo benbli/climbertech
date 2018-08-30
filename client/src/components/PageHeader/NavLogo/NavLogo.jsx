adc7a5edcbd6dc6309f5a4e7e202b1d23d8a8575
Daniel Lai <dlai813@gmail.com>
2018-08-30 16:25:06 -0700

    remove img folder

--- client/src/components/PageHeader/NavLogo/NavLogo.jsx
+++ client/src/components/PageHeader/NavLogo/NavLogo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./NavLogo.css"
+const path = require('path');
+
+const logoPath = path.join(__dirname, '/img/LogoCT.png');
 
 const NavLogo = props => {
   return (
     <a className={`${styles.logoAnchor} ${styles.logoWrapper}`}
       aria-label="Climber Tech, Back to Home" 
       href="/">
-      <img className={styles.logo} src="/Users/Yadleo/Desktop/CT2/climbertech/img/LogoCT.png" alt="Climber Tech"/>
+      <img className={styles.logo} src="http://res.cloudinary.com/dqhe5ks7u/image/upload/f_auto,q_auto/v1535671309/ClimberTech/LogoCT.png" alt="Climber Tech"/>
     </a>
   )
 }
