19f1d506399cc1860dbe14e621910baf9d10a8a8
Ben Li <benbli@gmail.com>
2019-02-20 10:44:00 -0800

    adds unique key to nav list

--- client/src/components/PageHeader/NavList/NavList.jsx
+++ client/src/components/PageHeader/NavList/NavList.jsx
@@ -1,28 +1,38 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import styles from "./NavList.css";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './NavList.css';
 
 export default class NavList extends Component {
   constructor(props) {
     super(props),
-    this.state = {
-      navListEntries: ['about', 'climbers', 'setters', 'gyms', 'events']
-    }
+      (this.state = {
+        navListEntries: ['about', 'climbers', 'setters', 'gyms', 'events'],
+      });
   }
 
   render() {
-    let navListStyle = this.props.expanded ? `${styles.navList} ${styles.navListShown}` : `${styles.navList}`;
+    let navListStyle = this.props.expanded
+      ? `${styles.navList} ${styles.navListShown}`
+      : `${styles.navList}`;
 
-    return(
-      <nav id="PageNavInterior"
+    return (
+      <nav
+        id="PageNavInterior"
         className={navListStyle}
         aria-hidden={!this.props.expanded}
-        aria-label="Primary navigation">
+        aria-label="Primary navigation"
+      >
         <ul className={styles.navListContainer}>
-          {this.state.navListEntries.map(entry => <li className={styles.navListEntry}><Link to={`/${entry}`} className={styles.navListEntryAnchor}>{entry}</Link></li>)}
+          {this.state.navListEntries.map((entry, i) => (
+            <li className={styles.navListEntry} key={i}>
+              <Link to={`/${entry}`} className={styles.navListEntryAnchor}>
+                {entry}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
-    )
+    );
   }
 }
 
